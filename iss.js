85b759963d4aedbd13f4b5f1248a18d1beb9552d
Todd Demone <tdemone1@gmail.com>
2021-11-19 19:15:37 +0000

    changed error conditional to mirror Compass answer

--- iss.js
+++ iss.js
@@ -9,8 +9,7 @@ const fetchMyIP = function(callback) {
     'https://api.ipify.org?format=json',
     function(error, response, body) {
       if (error) {
-        callback(error, null);
-        return;
+        return callback(error, null);
       }
       if (response.statusCode !== 200) {
         callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
