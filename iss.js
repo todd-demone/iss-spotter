b29d1a3283b410d5745bf3199bfc7762c818631c
Todd Demone <tdemone1@gmail.com>
2021-11-19 19:14:21 +0000

    complete part I of ISS project

--- iss.js
+++ iss.js
@@ -1,19 +1,23 @@
 const request = require('request');
 /**
- * Makes a single API request to retrieve the user's IP address. 
- * @param {Function} callback - to pass back an error or the IP string 
+ * Makes a single API request to retrieve the user's IP address.
+ * @param {Function} callback - to pass back an error or the IP string
  * returns (via Callback} an error, if any (nullable) OR the IP address as a string (null if error). Example "162.245.144.188"
  */
 const fetchMyIP = function(callback) {
   request(
     'https://api.ipify.org?format=json',
-    (error, response, body) => {
+    function(error, response, body) {
       if (error) {
-        callback(error, null)
-      } else {
-        const ip = JSON.parse(body).ip;
-        callback(null, ip);
+        callback(error, null);
+        return;
       }
+      if (response.statusCode !== 200) {
+        callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+        return;
+      }
+      const ip = JSON.parse(body).ip;
+      callback(null, ip);
     }
   );
 };
