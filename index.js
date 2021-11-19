d0cca2a52fd990da77f5fa23c141be017bd29802
Todd Demone <tdemone1@gmail.com>
2021-11-19 20:07:29 +0000

    complete Part II of ISS exercise M2W5

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP(
-  (error, ip) => {
-    if (error) {
-      console.log("It didn't work!", error);
-      return;
-    }
+// fetchMyIP(
+//   (error, ip) => {
+//     if (error) {
+//       console.log("It didn't work!", error);
+//       return;
+//     }
 
-    console.log("It worked! Returned IP:", ip);
-  }
-);
+//     console.log("It worked! Returned IP:", ip);
+//   }
+// );
+
+// fetchCoordsByIP(
+//   '104.247.231.19',
+//   function(error, data) {
+//     if (error) {
+//       console.log("It didn't work", error);
+//       return;
+//     }
+//     console.log("It worked! Returned coordinates:", data);
+//   }
+// );
--- iss.js
+++ iss.js
@@ -9,10 +9,12 @@ const fetchMyIP = function(callback) {
     'https://api.ipify.org?format=json',
     function(error, response, body) {
       if (error) {
-        return callback(error, null);
+        callback(error, null);
+        return;
       }
       if (response.statusCode !== 200) {
-        callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+        const msg = `Status Code ${response.statusCode} when fetching coordinates. Response ${body}`;
+        callback(Error(msg), null);
         return;
       }
       const ip = JSON.parse(body).ip;
@@ -21,4 +23,33 @@ const fetchMyIP = function(callback) {
   );
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the Geo coordinates of a given ip address
+ * @param {string} ip the ip address
+ * @param {Function} callback - to pass back an error or the coordinates
+ * Returns (via callback) an error, an object containing lat and lng (null if error)
+ * e.g., { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function(ip, callback) {
+  request(
+    `https://api.freegeoip.app/json/${ip}?apikey=4f548b80-4970-11ec-ba35-750e9fae5d6b`,
+    function(error, response, body) {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response ${body}`;
+        callback(Error(msg), null);
+        return;
+      }
+      const data = JSON.parse(body);
+      const coords = {};
+      coords.latitude = data.latitude;
+      coords.longitude = data.longitude;
+      callback(null, coords);
+    }
+  );
+};
+
+module.exports = { fetchCoordsByIP };
